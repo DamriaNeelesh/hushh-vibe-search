0243ea3716d780643400882551991edee1278341
Apoorv Bedmutha <i-apoorv@hushh.ai>
2024-10-14 19:52:19 +0530

    Searchbox working

--- src/app/components/Utilities/SearchBox/SearchBox.js
+++ src/app/components/Utilities/SearchBox/SearchBox.js
@@ -1,10 +1,15 @@
 import styles from "./SearchBox.module.css";
 import resources from "./resources/resources";
+import config from "../../../resources/config/config";
 export default function SearchBox() {
   return (
     <div className={`${styles.SearchBox}`}>
       <img src={resources.magnifyingGlass.src}></img>
-      <input className={`${styles.SearchBox__Input}`}></input>
+      <input className={`${styles.SearchBox__Input}`} onKeyDown={(event)=>{
+        if(event.key === "Enter"){
+          window.location.href = config.redirect_url+"/components/SearchResults?query=" + event.target.value;
+        }
+      }}></input>
       <img src={resources.cross.src}></img>
       <img src={resources.camera.src}></img>
     </div>
