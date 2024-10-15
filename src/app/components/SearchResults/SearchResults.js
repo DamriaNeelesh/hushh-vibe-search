c43ac8e1067ad86a5460528c24a9b1eff0254b29
DamriaNeelesh <damrianeelesh@gmail.com>
2024-10-15 20:46:03 +0530

    drawer with scrolling (ongoing)

--- src/app/components/SearchResults/SearchResults.js
+++ src/app/components/SearchResults/SearchResults.js
@@ -7,51 +7,40 @@ import {
   Text,
   VStack,
   HStack,
-  Checkbox,
   Slider,
   SliderTrack,
   SliderFilledTrack,
   SliderThumb,
-  IconButton,
-  Input,
   Avatar,
-  Icon,
   Grid,
   Image,
+  Button,
   Link,
-  useToast,
 } from "@chakra-ui/react";
 import { FiHeart, FiUser } from "react-icons/fi";
 import services from "../../services/services";
 import { useSearchParams } from "next/navigation";
 import FiltersAndHistory from "./FiltersAndHistory/FiltersAndHistory";
 import Footer from "../footer";
-import VibeText from "../svg/vibeText.svg";
 import styles from "./SearchResults.module.css";
 
 export default function SearchResults() {
-  let [searchResults, setSearchResults] = useState([]);
-  let [currentPage, setCurrentPage] = useState(1);
-  let [selectedBrands, setSelectedBrands] = useState([]);
-  let [noMoreResults, setNoMoreResults] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedBrands, setSelectedBrands] = useState([]);
+  const [noMoreResults, setNoMoreResults] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [isGridScrollAllowed, setIsGridScrollAllowed] = useState(true); // New state for page scroll control
   const searchParams = useSearchParams();
-  const toast = useToast();
 
   useEffect(() => {
-    let wishlist = searchParams.get("isWishlist");
-    if (wishlist) {
-      return;
-    }
     async function callVibeIt() {
       let search = searchParams.get("query");
       services.history.saveToHistory(search);
-      const isImageSearch = searchParams.get("imageSearch");
-      if (isImageSearch) {
-        search = localStorage.getItem("image-file");
-      }
       let access_token = await services.authentication.getAccessToken();
       services.vibesearch.vibeIt(
-        search ? search : "",
+        search || "",
         "",
         currentPage,
         20,
@@ -64,19 +53,41 @@ export default function SearchResults() {
     }
     callVibeIt();
   }, [searchParams, currentPage, selectedBrands, noMoreResults]);
+
+  const openDrawer = (product) => {
+    setSelectedProduct(product);
+    setIsDrawerOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setIsDrawerOpen(false);
+    setSelectedProduct(null);
+  };
+
+  // Handle scrolling control when mouse enters the grid or left filter area
+  const handleMouseEnterGrid = () => {
+    setIsGridScrollAllowed(true); // Allow grid to scroll when mouse is on the product grid
+  };
+
+  const handleMouseLeaveGrid = () => {
+    setIsGridScrollAllowed(false); // Disallow scrolling for the rest of the page
+  };
+
+  // Make sure the left section remains scrollable, even if the drawer is open
+  const handleLeftScroll = (event) => {
+    event.stopPropagation();
+    event.target.scrollTop = event.target.scrollTop; // Allow scrolling in the left section
+  };
+
+  // Prevent body scroll if the mouse is not over the grid
   useEffect(() => {
-    async function callGetWishlist() {
-      let access_token = await services.authentication.getAccessToken();
-      services.wishlist.getWishList(access_token, setSearchResults);
+    if (isDrawerOpen || !isGridScrollAllowed) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
     }
-    let wishlist = searchParams.get("isWishlist");
-    if (wishlist) {
-      callGetWishlist();
-    }
-  }, []);
-  useEffect(() => {
-    console.log(selectedBrands);
-  }, [selectedBrands]);
+  }, [isDrawerOpen, isGridScrollAllowed]);
+
   return (
     <>
       <Box
@@ -92,17 +103,18 @@ export default function SearchResults() {
           align="start"
           p={4}
           minW={{ base: "100%", md: "300px" }}
-          // bg="gray.100"
           h={"100vh"}
           position={"sticky"}
           borderRadius="md"
           top={"0"}
+          overflowY="auto" // Ensure the left section can scroll
+          onScroll={handleLeftScroll}
         >
           <div className={`${styles.SearchResults__Filters}`}>
             <FiltersAndHistory
               setSelectedBrands={setSelectedBrands}
               selectedBrands={selectedBrands}
-            ></FiltersAndHistory>
+            />
             <Text fontWeight="bold" fontSize="lg" mt={4}>
               PRICE
             </Text>
@@ -115,6 +127,7 @@ export default function SearchResults() {
             <Text>$10 - $1050</Text>
           </div>
         </VStack>
+
         {/* Main Content Section */}
         <Box
           bg={"white"}
@@ -124,6 +137,8 @@ export default function SearchResults() {
           pb={{ md: 4, base: 1 }}
           display="flex"
           flexDirection="column"
+          onMouseEnter={handleMouseEnterGrid} // Allow page scroll when mouse is over grid
+          onMouseLeave={handleMouseLeaveGrid} // Disallow page scroll when mouse leaves grid
         >
           {/* Header Section */}
           <HStack
@@ -141,100 +156,109 @@ export default function SearchResults() {
               <Text fontSize="2xl" fontWeight="bold">
                 Vibe
               </Text>
-              {/* <Image src={VibeText} objectFit="cover" alt="Vibe Search"/> */}
-              <utilities.SearchBox></utilities.SearchBox>
+              <utilities.SearchBox />
             </HStack>
             <HStack spacing={4}>
-              <Icon as={FiHeart} w={6} h={6} color="gray.500" onClick={()=>{
-                window.location.href = config.redirect_url+"/components/SearchResults?isWishlist=true";
-              }}/>
+              <FiHeart size={24} onClick={() => (window.location.href = "/wishlist")} />
               <Avatar icon={<FiUser />} bg="gray.500" />
             </HStack>
           </HStack>
 
-          {/* Product Results */}
-
-          <Grid
-            templateColumns={{ base: "1fr", md: "repeat(4, 1fr)" }}
-            gap={{ md: 14, base: 5 }}
-            mt={{ md: "2rem", base: "1rem" }}
-            mb={{ md: "6rem", base: "2rem" }}
-          >
-            {Object.values(searchResults).map((product, index) => (
-              <Box
-                key={index}
-                position="relative"
-                borderRadius="10px"
-                overflow="hidden"
-                bg="#F8F4F2"
-                minH="350px"
-                className="product-card"
-              >
-                <Box position="relative" className="image-container">
-                  <Link href={product.product_url} isExternal>
-                    <Image
-                      src={product.image || "/path/to/default-image.jpg"}
-                      alt={product.product_title}
-                      width="100%"
-                      minH="300px"
-                      maxH="300px"
-                      objectFit="cover"
-                    />
-                  </Link>
-                  <Box
-                    className="favorite-button"
-                    position="absolute"
-                    bottom="0"
+          <HStack display={"flex"} gap={{ md: 14, base: 5 }} alignItems={"flex-start"} flexDirection={"row"}>
+            {/* Product Results */}
+            <Grid
+              flex={1}
+              templateColumns={{ base: "1fr", md: isDrawerOpen ? "repeat(2, 1fr)" : "repeat(4, 1fr)" }}
+              gap={{ md: 14, base: 5 }}
+              mt={{ md: "2rem", base: "1rem" }}
+              mb={{ md: "6rem", base: "2rem" }}
+              overflowY="auto" // Allow grid scrolling
+              maxH="100vh" // Constrain grid to viewport height
+            >
+              {Object.values(searchResults).map((product, index) => (
+                <Box
+                  key={index}
+                  position="relative"
+                  borderRadius="10px"
+                  overflow="hidden"
+                  bg="#F8F4F2"
+                  minH="350px"
+                  onClick={() => openDrawer(product)}
+                  cursor="pointer"
+                >
+                  <Image
+                    src={product.image || "/path/to/default-image.jpg"}
+                    alt={product.product_title}
                     width="100%"
-                    bg="#624737"
-                    color="white"
-                    textAlign="center"
-                    p={2}
-                    transform="translateY(100%)"
-                    transition="transform 0.3s ease"
-                    cursor={"pointer"}
-                    onClick={async () => {
-                      let access_token =
-                        await services.authentication.getAccessToken();
-                      services.wishlist.addToWishList(product.id, access_token);
-                    }}
-                  >
-                    Add to Favorites
+                    minH="300px"
+                    maxH="300px"
+                    objectFit="cover"
+                  />
+                  <Box p={3} bg={"#F8F4F2"}>
+                    <Text fontWeight="600" color={"#000000"} noOfLines={1}>
+                      {product.product_title}
+                    </Text>
+                    <Text fontWeight="600" color="#757575" noOfLines={1}>
+                      {product.description}
+                    </Text>
+                    {product.price_available && (
+                      <Text fontWeight="600" color={"#9F9F9F"} noOfLines={1}>
+                        {product.currency} {product.price}
+                      </Text>
+                    )}
                   </Box>
                 </Box>
-                <Box p={3} bg={"#F8F4F2"}>
-                  <Text
-                    fontWeight="600"
-                    color={"#000000"}
-                    noOfLines={1}
-                    fontSize={{ md: "18px", base: "12px" }}
-                    mt={2}
-                  >
-                    {product.product_title}
+              ))}
+            </Grid>
+
+            {/* Right Sidebar for Product Details */}
+            {isDrawerOpen && (
+              <Box
+                width={{ base: "100%", md: "40%" }}
+                position="sticky"
+                top={0}
+                bg="white"
+                boxShadow="lg"
+                zIndex={10}
+                overflowY="auto"
+                height="100vh"
+              >
+                <HStack borderTopRadius={"10px"} p={4} bg={"#F4EFEB"} justifyContent={"space-between"}>
+                  <Text fontSize="2xl" fontWeight="bold">
+                    {selectedProduct?.product_title}
                   </Text>
-                  <Text
-                    fontSize={{ md: "14px", base: "9px" }}
-                    noOfLines={1}
-                    fontWeight={"600"}
-                    color="#757575"
-                  >
-                    {product.description}
-                  </Text>
-                  {product.price_available && (
-                    <Text
-                      fontWeight="600"
-                      color={"#9F9F9F"}
-                      noOfLines={1}
-                      fontSize={{ md: "15px", base: "8px" }}
-                      mt={2}
-                    >
-                      {product.currency} {product.price}
-                    </Text>
-                  )}
-                </Box>
+                  <svg onClick={closeDrawer} width="14" height="14" viewBox="0 0 14 14" fill="none">
+                    <path
+                      d="M1.4 14L0 12.6L5.6 7L0 1.4L1.4 0L7 5.6L12.6 0L14 1.4L8.4 7L14 12.6L12.6 14L7 8.4L1.4 14Z"
+                      fill="#9AA0A6"
+                    />
+                  </svg>
+                </HStack>
+
+                {selectedProduct && (
+                  <>
+                    <Image
+                      src={selectedProduct.image || "/path/to/default-image.jpg"}
+                      alt={selectedProduct.product_title}
+                      minW="100%"
+                      maxH={"621px"}
+                      objectFit="cover"
+                    />
+                    <Text fontWeight="bold">{selectedProduct.brand}</Text>
+                    <Text>{selectedProduct.description}</Text>
+                    {selectedProduct.price_available && (
+                      <Text fontWeight="bold" fontSize="lg">
+                        {selectedProduct.currency} {selectedProduct.price}
+                      </Text>
+                    )}
+                    <Button as={Link} href={selectedProduct.product_url} isExternal colorScheme="purple" width="full">
+                      Visit Product Page
+                    </Button>
+                  </>
+                )}
               </Box>
-            ))}
-          </Grid>
+            )}
+          </HStack>
         </Box>
       </Box>
       <Footer />
