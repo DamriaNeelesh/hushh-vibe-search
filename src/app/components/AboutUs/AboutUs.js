89908961038eaefc59cbf5193e075f128b4e41e8
Apoorv Bedmutha <i-apoorv@hushh.ai>
2024-10-16 15:36:22 +0530

    Add Links to ABout and Contact

--- src/app/components/AboutUs/AboutUs.js
+++ src/app/components/AboutUs/AboutUs.js
@@ -7,6 +7,7 @@ import YourTrust from "./YourTrust/YourTrust";
 import { Box } from "@chakra-ui/react";
 import Link from "next/link";
 import { Text } from "@chakra-ui/react";
+import config from "../../resources/config/config";
 export default function AboutUs() {
   return (
     <div>
@@ -26,9 +27,10 @@ export default function AboutUs() {
       <ValueProp image={Resources.images.AISearch.src} imageDirection={"right"}>
         <div>
           <div className={`${styles.AboutUs__Desc}`}>
-            Vibe Search is an <strong>AI-powered shopping search engine</strong> that uses
-            advanced <strong>semantic search</strong> technology to understand <strong>natural language
-            queries,</strong> delivering personalised experiences.
+            Vibe Search is an <strong>AI-powered shopping search engine</strong>{" "}
+            that uses advanced <strong>semantic search</strong> technology to
+            understand <strong>natural language queries,</strong> delivering
+            personalised experiences.
           </div>
         </div>
       </ValueProp>
@@ -41,10 +43,11 @@ export default function AboutUs() {
             Personalized Shopping<br></br> with Privacy
           </div>
           <div className={`${styles.AboutUs__DescSmall}`}>
-            Our platform ensures <strong>privacy protection</strong> and secure data handling,
-            using your information only with your explicit permission to enhance
-            your shopping experience, connecting you with the most relevant
-            products from <strong>global fashion brands online.</strong>
+            Our platform ensures <strong>privacy protection</strong> and secure
+            data handling, using your information only with your explicit
+            permission to enhance your shopping experience, connecting you with
+            the most relevant products from{" "}
+            <strong>global fashion brands online.</strong>
           </div>
         </div>
       </ValueProp>
@@ -57,8 +60,9 @@ export default function AboutUs() {
             Features That Surpass <br></br>Traditional Search
           </div>
           <div className={`${styles.AboutUs__DescSmall}`}>
-            With <strong>privacy-focused image search capabilities,</strong> you can find similar
-            products online even <strong>without exact keyword matches.</strong> Access our vast
+            With <strong>privacy-focused image search capabilities,</strong> you
+            can find similar products online even{" "}
+            <strong>without exact keyword matches.</strong> Access our vast
             catalog of global fashion brands and discover items that truly
             <strong>resonate with your style and intent.</strong>
           </div>
@@ -69,13 +73,25 @@ export default function AboutUs() {
         <div className={`${styles.AboutUs__UpperFooterText}`}>
           Powered by{" "}
           <strong>
-            <Link href={"https://hushh.ai"} target="_blank">hushh</Link>
+            <Link href={"https://hushh.ai"} target="_blank">
+              hushh
+            </Link>
           </strong>
         </div>
       </div>
       <div className={`${styles.AboutUs__UpperFooter} figtree`}>
-        <div className={`${styles.AboutUs__UpperFooterText}`}>About</div>
-        <div className={`${styles.AboutUs__UpperFooterText}`}>Contact us</div>
+        <Link
+          className={`${styles.AboutUs__UpperFooterText}`}
+          href={config.redirect_url + "/components/AboutUs"}
+        >
+          About
+        </Link>
+        <Link
+          className={`${styles.AboutUs__UpperFooterText}`}
+          href={"https://www.hush1one.com/contact-us"}
+        >
+          Contact us
+        </Link>
       </div>
     </div>
   );
