98babcd03dbb818b8ee12b9c6993834be5ba0f5d
DamriaNeelesh <damrianeelesh@gmail.com>
2024-10-20 20:53:04 +0530

    adding placeholder and initial query value on search box

--- src/app/components/utilities/SearchBox/SearchBox.js
+++ src/app/components/utilities/SearchBox/SearchBox.js
@@ -3,19 +3,31 @@ import styles from "./SearchBox.module.css";
 import resources from "./resources/resources";
 import config from "../../../resources/config/config";
 import FileInputBox from './FileInputBox/FileInputBox'
-//import { useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 export default function SearchBox() {
-  // let searchParams=useSearchParams()
-  // let query=searchParams.get('query')
+  let searchParams=useSearchParams()
+  let initialQuery=searchParams.get('query')
+  // console.log('Query:', query)
+  const [searchQuery, setSearchQuery] = useState(initialQuery);
+
+  const handleSearch = (event) => {
+    if (event.key === "Enter") {
+      window.location.href = config.redirect_url + "/components/SearchResults?query=" + searchQuery;
+    }
+  };
+
   return (
     <div className={`${styles.SearchBox}`}>
       <img src={resources.magnifyingGlass.src}></img>
-      <input className={`${styles.SearchBox__Input}`} id="SearchBox__Input" onKeyDown={(event)=>{
-        if(event.key === "Enter"){
-          window.location.href = config.redirect_url+"/components/SearchResults?query=" + event.target.value;
-        }
+      <input 
+        className={`${styles.SearchBox__Input}`}
+        id="SearchBox__Input"
+        value={searchQuery} // Set the value to the searchQuery state
+        onChange={(e) => setSearchQuery(e.target.value)} // Update the state on change
+        onKeyDown={handleSearch}
         // placeholder={query} 
-      }}></input>
+      ></input>
       <img src={resources.cross.src} className={`${styles.SearchBox__Icon}`} onClick={()=>{
         document.getElementById("SearchBox__Input").value = "";
       }}></img>
