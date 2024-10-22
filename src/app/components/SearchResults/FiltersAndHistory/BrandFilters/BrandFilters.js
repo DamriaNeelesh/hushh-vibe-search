a28455aacd92eafbc0208ecd77c03d08955bd8c4
DamriaNeelesh <damrianeelesh@gmail.com>
2024-10-22 17:36:12 +0530

    adding show more and show less buton on brand filters

--- src/app/components/SearchResults/FiltersAndHistory/BrandFilters/BrandFilters.js
+++ src/app/components/SearchResults/FiltersAndHistory/BrandFilters/BrandFilters.js
@@ -1,9 +1,14 @@
+'use client'
+import { useState } from "react";
 import config from "../../../../resources/config/config";
 import styles from "./BrandFilters.module.css";
 export default function BrandFilters({ selectedBrands, setSelectedBrands, brands }) {
+  const [showAll, setShowAll] = useState(false);
+  const displayedBrands = showAll ? brands : brands.slice(0, 10);
+
   return (
     <div className={`${styles.BrandFilters}`}>
-      {brands.map((brand, index) => (
+      {displayedBrands.map((brand, index) => (
         <div key={index}>
           <input
             type="checkbox"
@@ -20,6 +25,18 @@ export default function BrandFilters({ selectedBrands, setSelectedBrands, brands
           &nbsp; {brand}
         </div>
       ))}
+      {/* Show More button if there are more than 10 brands */}
+      {brands.length > 10 && !showAll && (
+        <div onClick={() => setShowAll(true)} className={styles.showMoreButton}>
+          Show More
+        </div>
+      )}
+      {/* Show Less button to collapse the list */}
+      {showAll && (
+        <button onClick={() => setShowAll(false)} className={styles.showMoreButton}>
+          Show Less
+        </button>
+      )}
     </div>
   );
 }
