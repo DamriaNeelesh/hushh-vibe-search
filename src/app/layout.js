e33a538917ed00afebd1a074d2bd18dbaaa3144e
DamriaNeelesh <damrianeelesh@gmail.com>
2024-10-22 19:04:17 +0530

    google tag manager setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -37,6 +37,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+{/* Google Tag Manager */}
+<script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N3KQ5QKZ');
+            `,
+          }}
+        />
+
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-JG6C3FQ2N8	"></script>
 <script
           dangerouslySetInnerHTML={{
@@ -83,6 +96,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N3KQ5QKZ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
         <ChakraBaseProvider><ChakraProvider>{children}</ChakraProvider></ChakraBaseProvider>
       </body>
     </html>
