3916a23411b9d3d5264ffbdf65dfe4f8ddd224c2
Apoorv Bedmutha <i-apoorv@hushh.ai>
2024-10-23 17:53:36 +0530

    Infinite Scroll in Search Results + Modularising Search Results Page

--- src/app/components/utilities/utilities.js
+++ src/app/components/utilities/utilities.js
@@ -1,8 +1,10 @@
+import ComingSoonToast from "./ComingSoonToast/ComingSoonToast";
 import Footer from "./Footer/Footer";
 import SearchBox from "./SearchBox/SearchBox";
 
 const utilities={
     SearchBox: SearchBox,
-    Footer: Footer
+    Footer: Footer,
+    ComingSoonToast: ComingSoonToast
 }
 export default utilities;
