b3a0e1da510da7092394b70474daf85320453bfe
DamriaNeelesh <damrianeelesh@gmail.com>
2024-10-30 10:35:51 +0530

    layout blog UI

--- src/app/blogs/latest-blogs/page.js
+++ src/app/blogs/latest-blogs/page.js
@@ -1,7 +1,13 @@
+import Header from '../components/Header';
 import BlogDetailPage from './BlogDetailPage';
 
 const BlogDetail = () => {
-  return <BlogDetailPage />;
+  return (
+  <>
+     <Header/>
+     <BlogDetailPage />
+  </> 
+  )
 };
 
 export default BlogDetail;
